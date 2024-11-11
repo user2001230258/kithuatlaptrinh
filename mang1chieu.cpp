0345e90855ffcf650d6fa911ce66c376229b1db6
Hoang Van Hoan <2001230258@hufi.edu.vn>
2024-11-12 00:00:25 +0700

    Update mang1chieu.cpp

--- mang1chieu.cpp
+++ mang1chieu.cpp
@@ -151,9 +151,39 @@ void soNguyenTo(int *a, int n) {
     }
     printf("\n");
 }
+
+int linearSearch(int *a,int n, int x) {
+    for (int i = 0; i < n;i++) {
+        if (a[i] == x) {
+            return i; // tra ve chi so phan tu tim thay
+        }
+    }
+    return -1; // tra ve -1 neu k tim thay
+}
+
+int binarySearch(int *a, int left, int right, int x) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        
+        // kiem tra neu phan tu mid la khoa tim kiem
+        if (a[mid] == x) {
+            return mid;
+        }
+        // neu x lon hon, bo qua nua trai
+        if (a[mid] < x) {
+            left = mid + 1;
+        }
+        // neu x nho hon, bo qua nua phai
+        else {
+            right = mid -1;
+        }
+    }
+    return -1;
+}
+
 int main()
 {
-    int n,x;
+    int n,x,key;
     int *a; 
     printf("Nhap so phan tu cua mang: "); scanf("%d",&n);
     
@@ -163,14 +193,29 @@ int main()
     //interchangeSort(a,n);
     //selectionSort(a,n);
     //insertionSort(a,n);
-    quickSort(a,0,n - 1);
-    printf("Nhap x: "); scanf("%d",&x);
-    themPhanTu(&a,&n,x);
+    //quickSort(a,0,n - 1);
+    //printf("Nhap x: "); scanf("%d",&x);
+    //themPhanTu(&a,&n,x);
     printf("Mang: ");
     xuatMang(a,n);
-    printf("Max = %d\n",timMax(a,n));
-    printf("Min = %d\n",timMin(a,n));
-    soNguyenTo(a,n);
+    //printf("Max = %d\n",timMax(a,n));
+    //printf("Min = %d\n",timMin(a,n));
+    //soNguyenTo(a,n);
+    printf("Nhap phan tu can tim: "); scanf("%d",&key);
+    /*int result = linearSearch(a,n,key);
+    if (result != -1) {
+        printf("Gia tri %d duoc tim thay tai chi so a[%d]\n",key,result);
+    } else {
+        printf("Gia tri %d khong ton tai trong mang!",key);
+    }*/
+    
+    int timKiem = binarySearch(a,0,n-1,key);
+    if (timKiem != -1) {
+        printf("Gia tri %d duoc tim thay tai chi so a[%d]", key, timKiem);
+    } else {
+        printf("Gia tri %d k ton tai trong mang!", key);
+    }
+    
     free(a);
    
     return 0;

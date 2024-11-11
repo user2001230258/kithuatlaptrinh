697497682449fe357e96fe7826979bbd3d0fa9ca
Hoang Van Hoan <2001230258@hufi.edu.vn>
2024-11-11 16:37:33 +0700

    Update matranvuong.cpp

--- matranvuong.cpp
+++ matranvuong.cpp
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 void nhapMaTran(int **a, int n) {
     for (int i = 0; i < n; i++) {
@@ -106,6 +107,97 @@ void xuatCacDuongCheoSSVoiDuongCheoPhu(int **a, int n) {
     }
 }
 
+void xuatPhanTuHoangHau(int **a, int n) {
+    printf("Cac phan tu hoang hau:\n");
+    for (int i =0; i <n;i++) {
+        for (int j = 0 ;j < n;j++) {
+            bool hoangHau = true;
+            for (int k = 0; k < n;k++) {
+                if (a[i][j] < a[i][k] || a[i][j] < a[k][j]) {
+                    hoangHau = false;
+                    break;
+                }
+            }
+            if (hoangHau) {
+                printf("a[%d][%d] = %d\n",i,j,a[i][j]);
+            }
+        }
+    }
+}
+
+void xuatPhanTuYenNgua(int **a, int n) {
+    printf("Cac phan tu yen ngua:\n");
+    for (int i =0; i < n;i++) {
+        for (int j = 0; j < n;j++) {
+            bool yenNgua = true;
+            // kiem tra nho nhat tren hang
+            for (int k = 0;k < n;k++) {
+                if (a[i][j] > a[i][k]) {
+                    yenNgua = false;
+                    break;
+                }
+            }
+            // kiem tra lon nhat tren cot 
+            for (int k = 0; k < n;k++) {
+                if (a[i][j] < a[k][j]) {
+                    yenNgua = false;
+                    break;
+                }
+            }
+            if (yenNgua) {
+                printf("a[%d][%d] = %d\n",i,j,a[i][j]);
+            }
+        }
+    }
+}
+
+bool kiemTraCucDai(int **a, int n, int i, int j) {
+    // Duyet qua cac phan tu lan can
+    for (int x = i -1; x <= i+1;x++) {
+        for (int y = j - 1; y <= j +1;y++) {
+            if (x >= 0 && x < n && y >=0 && y < n) { // kiem tra trong bien
+                if (a[i][j] < a[x][y]) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+bool kiemTraCucTieu(int **a ,int n, int i, int j) {
+    for (int x = i - 1; x <= i+1;x++) {
+        for (int y = j - 1; y <= j + 1;y++) {
+            if (x>= 0&& x < n && y >= 0 && y < n) { // kiem tra trong bien
+                if (a[i][j] > a[x][y]) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+void xuatPhanTuCucDaiVaCucTieu(int **a, int n) {
+    printf("Cac phan tu cuc dai:\n");
+    for (int i =0; i < n;i++) {
+        for (int j =0; j < n;j++) {
+            if (kiemTraCucDai(a,n,i,j)) {
+                printf("a[%d][%d] = %d\n",i,j,a[i][j]);
+            }
+        }
+    }
+    
+    printf("Cac phan tu cuc tieu:\n");
+    for (int i =0; i < n;i++) {
+        for (int j = 0; j < n;j++) {
+            if (kiemTraCucTieu(a,n,i,j)) {
+                printf("a[%d][%d] = %d\n",i,j,a[i][j]);
+            }
+        }
+    }
+}
+
 int main() {
     int n;
     int **a;
@@ -123,13 +215,16 @@ int main() {
     printf("Ma tran vuong:\n");
     xuatMaTran(a, n);
     
-    xuatCacDuongCheoSSVoiDuongCheoChinh(a,n);
-    xuatCacDuongCheoSSVoiDuongCheoPhu(a,n);
-
+    //xuatCacDuongCheoSSVoiDuongCheoChinh(a,n);
+    //xuatCacDuongCheoSSVoiDuongCheoPhu(a,n);
     //printf("Tong tam giac tren duong cheo chinh: %d\n", tongTamGiacTrenDuongCheoChinh(a, n));
     //printf("Tong tam giac duoi duong cheo chinh: %d\n", tongTamGiacDuoiDuongCheoChinh(a, n));
     //printf("Tong duong cheo chinh: %d\n",tongDuongCheoChinh(a,n));
     //printf("Tong duong cheo phu: %d\n",tongDuongCheoPhu(a,n));
+    
+    xuatPhanTuHoangHau(a,n);
+    xuatPhanTuYenNgua(a,n);
+    xuatPhanTuCucDaiVaCucTieu(a,n);
     // Giải phóng bộ nhớ
     for (int i = 0; i < n; i++) {
         free(a[i]);
